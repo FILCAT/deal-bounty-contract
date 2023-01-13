958bb90e55c1c97c6e4e2f4eeff14b574526ed39
zenground0 <ZenGround0@users.noreply.github.com>
2023-01-13 16:19:02 -0700

    call_actor takes value

--- src/DealRewarder.sol
+++ src/DealRewarder.sol
@@ -18,6 +18,8 @@ contract DealRewarder {
         owner = msg.sender;
     }
 
+    function fund() public payable {}
+
     function addCID(bytes calldata cidraw, uint size) public {
        require(msg.sender == owner);
        cidSet[cidraw] = true;
@@ -51,18 +53,58 @@ contract DealRewarder {
         send(clientRet.client);
     }
 
+    function concat(bytes32 b1, bytes32 b2) pure external returns (bytes memory)
+    {
+        bytes memory result = new bytes(64);
+        assembly {
+            mstore(add(result, 32), b1)
+            mstore(add(result, 64), b2)
+        }
+        return result;
+}
+
     function toBytes64(uint64 x) internal pure returns (bytes memory b) {
         b = new bytes(32);
         assembly { mstore(add(b, 32), x) }
     }
 
-
-    function send(uint64 actor_id) public {
+    // send 1 FIL to the filecoin actor at actor_id
+    function send(uint64 actor_id) public payable {
         uint METHOD_SEND = 0;
         bytes memory emptyParams = "";
         delete emptyParams;
-        Actor.call(METHOD_SEND, toBytes64(actor_id), emptyParams, Misc.NONE_CODEC);
+        bytes memory sendAddr = hex"0001";
+
+        uint oneFIL = 10000;
+        Actor.call_inner(METHOD_SEND, sendAddr, emptyParams, Misc.NONE_CODEC, oneFIL);
+
+        // handle ethereum transfers too?
     }
 
+
+    function slice_uint8(bytes memory bs, uint start) pure internal returns (uint8) {
+        require(bs.length >= start + 1, "slicing out of range");
+        uint8 x;
+        assembly {
+            x := mload(add(bs, add(0x01, start)))
+        }
+        return x;
+    }
+
+/*
+func PutUvarint(buf []byte, x uint64) int {
+	i := 0
+	for x >= 0x80 {
+		buf[i] = byte(x) | 0x80
+		x >>= 7
+		i++
+	}
+	buf[i] = byte(x)
+	return i + 1
+}
+*/
+
+// actor id => valid id address bytes
+// make a deal and see if we can actually claim bounty
 }
 
